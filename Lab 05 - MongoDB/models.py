0629dbb4f6cbb3d2648aa2c48191e4e0e192b319
Theo Crithary <theo@apacit.com.au>
2019-06-10 23:22:39 +1000

    .

--- Lab 05 - MongoDB/models.py
+++ Lab 05 - MongoDB/models.py
@@ -2,8 +2,8 @@
 
 #########################################################
 # This is the database processing file. (aka. Models)
-# It contains the DB connections, queries and processes
-# principles of Models, Views, Controllers (MVC).
+# It contains the DB connections, queries and processes.
+# Uses principles of Models, Views, Controllers (MVC).
 #########################################################
 
 # Import modules required for app
@@ -11,17 +11,9 @@ import os
 from pymongo import MongoClient
 from werkzeug import secure_filename
 
-# Check if running in Pivotal Web Services with MongoDB service bound
-if 'VCAP_SERVICES' in os.environ:
-    VCAP_SERVICES = json.loads(os.environ['VCAP_SERVICES'])
-    MONGOCRED = VCAP_SERVICES["mlab"][0]["credentials"]
-    client = MongoClient(MONGOCRED["uri"])
-    DB_NAME = str(MONGOCRED["uri"].split("/")[-1])
-
-# Otherwise, assume running locally with local MongoDB instance    
-else:
-    client = MongoClient('127.0.0.1:27017')
-    DB_NAME = "mongodb"  ##### Make sure this matches the name of your MongoDB database ######
+# Set the database target to your local MongoDB instance
+client = MongoClient('127.0.0.1:27017')
+DB_NAME = "mongodb"  ##### Make sure this matches the name of your MongoDB database ######
 
 # Get database connection with database name
 db = client[DB_NAME]
